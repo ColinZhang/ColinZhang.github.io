fe75c6fc9cc80ca7847559a0bc2bb984ae70d499
Fan ZHANG <hello.zhang1992@gmail.com>
2014-04-15 01:49:09 +0800

    Site updated 2014年 4月15日 星期二 01时49分09秒 CST

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,37 +2,54 @@
  * Created by fan on 14-2-9.
  */
 
-$(document).ready(function () {
-    /*
-    var color = [];
-    color[0] = '#b58900';
-    color[1] = '#cb4b16';
-    color[2] = '#dc322f';
-    color[3] = '#d33682';
-    color[4] = '#6c71c4';
-    color[5] = '#268bd2';
-    color[6] = '#2aa198';
-    color[7] = '#859900';
+//$(document).ready(function () {
+//    /*
+//    var color = [];
+//    color[0] = '#b58900';
+//    color[1] = '#cb4b16';
+//    color[2] = '#dc322f';
+//    color[3] = '#d33682';
+//    color[4] = '#6c71c4';
+//    color[5] = '#268bd2';
+//    color[6] = '#2aa198';
+//    color[7] = '#859900';
+//
+//
+//    $("#nav a").each(function(){
+//        var t = Math.floor(Math.random() * color.length);
+//        $(this).css("color", color[t]);
+//    });
+//
+//    $("#nav a").hover(function(){
+//            var t = Math.floor(Math.random() * color.length);
+//            $(this).css("color", color[t]);
+//        },
+//        function () {
+//        }
+//    );
+//    */
+//
+//    $("aside > section > ul.sns a").hover(function(){
+//            $(this).toggleClass('sns-hover');
+//        }
+//    );
+//});
 
 
-    $("#nav a").each(function(){
-        var t = Math.floor(Math.random() * color.length);
-        $(this).css("color", color[t]);
+$("document").ready(function($){
+
+    var nav = $('nav');
+    var width = nav.css("width");
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 125) {
+            nav.addClass("fixed-nav");
+            nav.css("width", width);
+        } else {
+            nav.removeClass("fixed-nav");
+        }
     });
 
-    $("#nav a").hover(function(){
-            var t = Math.floor(Math.random() * color.length);
-            $(this).css("color", color[t]);
-        },
-        function () {
-        }
-    );
-    */
-
-    $("aside > section > ul.sns a").hover(function(){
-            $(this).toggleClass('sns-hover');
-        }
-    );
 });
 
 
