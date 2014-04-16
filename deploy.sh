bf74cfd60616bbecf1fad2b45d1a6ae0f656f0ac
Fan ZHANG <fan.o.zhang@intel.com>
2014-04-16 12:40:52 +0800

    Site updated Wed Apr 16 12:40:52 CST 2014

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+cd ..
+rake generate
+cd -
+
+[[ "$(basename $(pwd))" == '_deploy' ]] || { 
+    echo Error
+    exit -1;
+}
+
+cp -rf ../public/* .
+git add --all
+git commit -m "Site updated `date`"
+git push
+
+
