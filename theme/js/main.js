7f77cd1ac54f740361cb21bad83dbf923cc15a22
Fan ZHANG <hello.zhang1992@gmail.com>
2014-04-21 00:36:09 +0800

    Site updated 2014年 4月21日 星期一 00时36分09秒 CST

--- theme/js/main.js
+++ theme/js/main.js
@@ -5,19 +5,7 @@
 $("document").ready(function($){
     /* Sticky Navigation Bar */
 
-    $(window).scroll(function () {
-        if ($("body").height() - $(window).height() < 200){
-            return;
-        }
 
-        if ($(this).scrollTop() > 125) {
-            $("body").addClass("nav-fixed");
-            $("nav").css("width", $("nav").css("width"));
-        } else {
-            $("body").removeClass("nav-fixed");
-
-        }
-    });
 
 });
 
--- theme/js/octopress.js
+++ theme/js/octopress.js
@@ -123,6 +123,22 @@ function renderDeliciousLinks(items) {
   $('#delicious').html(output);
 }
 
+function stickNavigator(){
+    $(window).scroll(function () {
+        if ($("body").height() - $(window).height() < 200){
+            return;
+        }
+
+        if ($(this).scrollTop() > 125) {
+            $("body").addClass("nav-fixed");
+            $("nav").css("width", $("nav").css("width"));
+        } else {
+            $("body").removeClass("nav-fixed");
+
+        }
+    });
+}
+
 $.domReady(function() {
   testFeatures();
   wrapFlashVideos();
@@ -130,6 +146,7 @@ $.domReady(function() {
   addCodeLineNumbers();
   getNav();
   addSidebarToggler();
+  stickNavigator();
 });
 
 // iOS scaling bug fix
