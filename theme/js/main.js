67a6130834de87598116468b7dbd7f1890612452
Fan ZHANG <hello.zhang1992@gmail.com>
2014-04-21 00:33:41 +0800

    Site updated 2014年 4月21日 星期一 00时33分41秒 CST

--- theme/js/main.js
+++ theme/js/main.js
@@ -4,18 +4,15 @@
 
 $("document").ready(function($){
     /* Sticky Navigation Bar */
-    width = $("nav").css("width");
 
     $(window).scroll(function () {
-
         if ($("body").height() - $(window).height() < 200){
             return;
         }
 
         if ($(this).scrollTop() > 125) {
             $("body").addClass("nav-fixed");
-            $("nav").css("width", width);
-
+            $("nav").css("width", $("nav").css("width"));
         } else {
             $("body").removeClass("nav-fixed");
 
